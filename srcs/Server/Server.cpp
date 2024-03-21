f8cd84caf198c06fa371702715524715f12a9e84
B4L00 <34213508+B4L00@users.noreply.github.com>
2024-03-21 11:13:17 +0100

    re organize server cpp ok

--- srcs/Server/Server.cpp
+++ srcs/Server/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: larmenou <larmenou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 08:42:29 by larmenou          #+#    #+#             */
-/*   Updated: 2024/03/21 08:34:40 by larmenou         ###   ########.fr       */
+/*   Updated: 2024/03/21 11:12:08 by larmenou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,6 @@ Server::Server(std::vector<ServerConf> servers) : _servers(servers), _sockets_li
 	
 	signal(SIGINT, &Server::signalHandler);
 	signal(SIGQUIT, &Server::signalHandler);
-
-	_status_code.insert(std::pair<int, std::string>(200, "OK"));
-	_status_code.insert(std::pair<int, std::string>(201, "Created"));
-	_status_code.insert(std::pair<int, std::string>(301, "Moved Permanently"));
-	_status_code.insert(std::pair<int, std::string>(401, "Unauthorized"));
-	_status_code.insert(std::pair<int, std::string>(404, "Not Found"));
-	_status_code.insert(std::pair<int, std::string>(413, "Payload Too Large"));
 
 	for (unsigned int i = 0; i < servers.size(); i++)
 	{
@@ -90,24 +83,67 @@ void Server::closeServer()
 		close(_sockets_listen[i]);
 }
 
-void Server::loop()
+void Server::initPollfds(std::vector<pollfd> *pollfds)
 {
-	int bytesReceived;
-	int ready;
-	int client_fd;
-	char buffer[BUFF_SIZE];
-	RegisteredUsers users;
-	
-	std::vector<pollfd> pollfds;
-	
-    for (unsigned int i = 0; i < _sockets_listen.size(); i++)
+	for (unsigned int i = 0; i < _sockets_listen.size(); i++)
 	{
         pollfd pfd;
         pfd.fd = _sockets_listen[i];
         pfd.events = POLLIN;
-        pollfds.push_back(pfd);
+        pollfds->push_back(pfd);
     }
+}
+
+void Server::addPollfd(std::vector<pollfd> *pollfds, int client_fd, int i)
+{
+	pollfd new_pollfd;
+	new_pollfd.fd = client_fd;
+	new_pollfd.events = POLLIN;
+	pollfds->insert(pollfds->begin() + i + 1, new_pollfd);
+}
+
+void Server::recvDataAndBuildResp(int client_fd, int i)
+{
+	char buffer[BUFF_SIZE];
+	int bytesReceived;
+	RegisteredUsers users;
 	
+	bytesReceived = read(client_fd, buffer, BUFF_SIZE - 1);
+	if (bytesReceived > 0)
+	{
+		buffer[bytesReceived] = '\0';
+		std::cout << "------ Received Request from client ------\n\n";
+
+		std::cout << buffer << std::endl;
+
+		std::stringstream ss(buffer);
+		std::string str = ss.str();
+		try
+		{
+			Request req(str);
+			if (_servers[i].getBodySizeLimit() > req.getBody().size())
+			{
+				std::string str = req.getBody();
+				users.addDb(str);
+			}
+			buildResponse(req, i, client_fd);
+		} catch (std::exception &e)
+		{
+			std::stringstream http;
+			_body_response = HTTPError::buildErrorPage(_servers[i], 400);
+			http << "HTTP/1.1" << " " << 400 << " " << HTTPError::getErrorString(400) << "\r\nContent-Type: text/html\r\nContent-Length: " << _body_response.length() << "\r\n";
+			sendResponse(client_fd);
+		}
+	}
+}
+
+void Server::loop()
+{
+	int ready;
+	int client_fd;
+	std::vector<pollfd> pollfds;
+	
+	initPollfds(&pollfds);
 	while (true)
 	{
 		std::cout << "====== Waiting for a new connection ======\n\n\n";
@@ -128,38 +164,8 @@ void Server::loop()
 				acceptConnection(client_fd, i);
 				if (client_fd != -1)
 				{
-					pollfd new_pollfd;
-					new_pollfd.fd = client_fd;
-					new_pollfd.events = POLLIN;
-					pollfds.insert(pollfds.begin() + i + 1, new_pollfd);
-					
-					bytesReceived = read(client_fd, buffer, BUFF_SIZE - 1);
-					if (bytesReceived > 0)
-					{
-						buffer[bytesReceived] = '\0';
-						std::cout << "------ Received Request from client ------\n\n";
-
-						std::cout << buffer << std::endl;
-
-						std::stringstream ss(buffer);
-						std::string str = ss.str();
-						try
-						{
-							Request req(str);
-							if (_servers[i].getBodySizeLimit() > req.getBody().size())
-							{
-								std::string str = req.getBody();
-								users.addDb(str);
-							}
-							buildResponse(req, i, client_fd);
-						} catch (std::exception &e)
-						{
-							std::stringstream http;
-							_body_response = HTTPError::buildErrorPage(_servers[i], 400);
-							http << "HTTP/1.1" << " " << 400 << " " << HTTPError::getErrorString(400) << "\r\nContent-Type: text/html\r\nContent-Length: " << _body_response.length() << "\r\n";
-							sendResponse(client_fd);
-						}
-					}
+					addPollfd(&pollfds, client_fd, i);
+					recvDataAndBuildResp(client_fd, i);
 					close(client_fd);
 					pollfds.erase(pollfds.begin() + i + 1);
 				}
@@ -198,12 +204,89 @@ static bool	fileExists(std::string path)
 	return true;
 }
 
+int Server::buildCgiResp(std::string *headers, Request req, Route route, int i)
+{
+	CGI cgi;
+	int status;
+
+	cgi.setCGI("/usr/bin/php-cgi");
+	cgi.prepare(req,route,_servers[i],"127.0.0.1");
+	try {
+		cgi.forwardReq();
+		_body_response = cgi.getBody();
+		status = cgi.getStatus();
+		*headers = cgi.buildRawHeader();
+	} catch (std::exception &e) {
+		_body_response = HTTPError::buildErrorPage(_servers[i], 
+													status = std::strtol(e.what(), NULL, 10));
+	}
+	return (status);
+}
+
+void Server::buildRedirResp(Route route, int client_fd)
+{
+	int status;
+	std::stringstream http;
+	
+	status = route.getRedirCode();
+	http << "HTTP/1.1" << " " << status << " " << HTTPError::getErrorString(status) << "\r\nLocation: " << route.getRewrite().second << "\r\nContent-Length: 0\r\n";
+	_header_response = http.str();
+	
+	send(client_fd, _header_response.c_str(), _header_response.size(), 0);
+}
+
+int Server::buildBodyResp(std::string filename, Request req, Route route, int i)
+{
+	int status = 200;
+	int fd;
+	
+	if (isDir(filename) && route.isListingDirs())
+		_body_response = DirLister().generate_body(filename, req);
+	else if (fileExists(filename))
+	{
+		fd = open(filename.c_str(), O_RDONLY);
+		if (fd == -1)
+			_body_response = HTTPError::buildErrorPage(_servers[i], status = 403);
+		char c;
+		while (read(fd, &c, 1) > 0)
+			_body_response += c;
+		close(fd);
+	}
+	else
+		_body_response = HTTPError::buildErrorPage(_servers[i], status = 404);
+	return (status);
+}
+
+std::string Server::buildFilename(Route route, Request req)
+{
+	std::string filename;
+	
+	filename = route.getRoot();
+	filename += req.getURN().substr(route.getRoute().length() - 1);
+	if (filename == route.getRoot() + "/")
+		filename += route.getDirFile();
+	return (filename);
+}
+
+void Server::buildHeaderConnection(std::string headers, Request req, std::stringstream *http)
+{
+	if (req.getHeaders().find("connection")->second == "keep-alive")
+	{
+		*http << "Connection: keep-alive\r\n" << headers << "\r\n";
+		_header_response = http->str();
+	}
+	else
+	{
+		*http << "Connection: close\r\n" << headers << "\r\n";
+		_header_response = http->str();
+	}
+}
+
 void Server::buildResponse(Request req, int i, int client_fd)
 {
 	std::stringstream http;
 	std::string filename;
 	std::string headers;
-	int fd;
 	int status = 200;
 	const Route &route = _servers[i].findRouteFromURN(req.getURN());
 
@@ -213,83 +296,33 @@ void Server::buildResponse(Request req, int i, int client_fd)
 	{	
 		if (_servers[i].getBodySizeLimit() <= req.getBody().size())
 		{
-			status = 413;
 			_body_response = HTTPError::buildErrorPage(_servers[i], status = 413);
 		}
 		else
 		{
 			if (route.getRoot().size() > 0)
 			{
-				filename = route.getRoot();
-				filename += req.getURN().substr(route.getRoute().length() - 1);
-				if (filename == route.getRoot() + "/")
-					filename += route.getDirFile();
+				filename = buildFilename(route, req);
 			}
 			else if (route.getRewrite().second.size() > 0)
 			{
-				status = route.getRedirCode();
-				http << "HTTP/1.1" << " " << status << " " << _status_code[status] << "\r\nLocation: " << route.getRewrite().second << "\r\nContent-Length: 0\r\n";
-				_header_response = http.str();
-				
-				send(client_fd, _header_response.c_str(), _header_response.size(), 0);
+				buildRedirResp(route, client_fd);
 				return ;
-			}
-			else
-			{
-				filename = _default_root;
-				filename += req.getURN();
-				if (filename == _default_root + "/")
-					filename += "index.html";
 			}
 
 			if (req.checkExtension(route.getCgiExtension()))
 			{
-				CGI cgi;
-
-				cgi.setCGI("/usr/bin/php-cgi");
-				cgi.prepare(req,route,_servers[i],"127.0.0.1");
-				try {
-					cgi.forwardReq();
-					_body_response = cgi.getBody();
-					status = cgi.getStatus();
-					headers = cgi.buildRawHeader();
-				} catch (std::exception &e) {
-					_body_response = HTTPError::buildErrorPage(_servers[i], 
-																status = std::strtol(e.what(), NULL, 10));
-				}
+				status = buildCgiResp(&headers, req, route, i);
 			}
 			else
 			{
-				if (isDir(filename) && route.isListingDirs())
-					_body_response = DirLister().generate_body(filename, req);
-				else if (fileExists(filename))
-				{
-					fd = open(filename.c_str(), O_RDONLY);
-					if (fd == -1)
-						_body_response = HTTPError::buildErrorPage(_servers[i], status = 403);
-					char c;
-					while (read(fd, &c, 1) > 0)
-						_body_response += c;
-					close(fd);
-				}
-				else
-					_body_response = HTTPError::buildErrorPage(_servers[i], status = 404);
-
+				status = buildBodyResp(filename, req, route, i);
 			}
 		}
 		http << "HTTP/1.1" << " " << status << " " << HTTPError::getErrorString(status) << "\r\nContent-Type: text/html\r\nContent-Length: " << _body_response.length() << "\r\n";
 	}
 	
-	if (req.getHeaders().find("Connection")->second == " keep-alive")
-	{
-		http << "Connection: keep-alive\r\n" << headers << "\r\n";
-		_header_response = http.str();
-	}
-	else
-	{
-		http << "Connection: close\r\n" << headers << "\r\n";
-		_header_response = http.str();
-	}
+	buildHeaderConnection(headers, req, &http);
 	
 	http.clear();
 	http.str("");
